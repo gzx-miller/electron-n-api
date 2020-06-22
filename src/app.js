d163c56e1b3bec52929729f8eecf1841f36e5a1c
guozhenxing <guozhenxing@qiyi.com>
2020-06-22 17:29:04 +0800

    add show

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var app = {
     run: function() {
         console.log("RetStr: " + napi.RetStr());
         console.log("Add: " + napi.Add(1, 2)); 
-        napi.RunCallback(function(msg){
+        napi.RunCallback((msg) => {
             console.log("RunCallback: " + msg);
         });
         
@@ -13,6 +13,31 @@ var app = {
         
         var func = napi.CreateFunction();
         console.log("CreateFunction: " + func());
+        
+        var fetcher1 = napi.CreateFetcher();
+        let promise1 = fetcher1.fetch();
+        promise1.then(data => {
+            console.log("Promise1.0.then data param: " + data);
+        });
+        promise1.then((data)=>{
+            console.log("Promise1.then data param: " + data);
+            return data + 1;
+        }).then((data)=>{
+            console.log("Promise1.2.then data param: " + data);
+            return data + 1;
+        });
+        
+        var fetcher2 = napi.CreateFetcher();
+        fetcher2.fetch().then((data)=>{
+            console.log("Promise2.then data param: " + data);
+            return data + 1;
+        });
+        
+        var fetcher3 = napi.CreateFetcher();
+        fetcher3.fetch().then((data)=>{
+            console.log("Promise3.then data param: " + data);
+            return data + 1;
+        });
     }
 }
 
